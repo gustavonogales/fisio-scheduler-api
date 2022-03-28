c53f3fc0e42f4f4c2480b8fede2509ac7f75952a
Gustavo Oquendo Nogales <gustavo.nogales@hotmail.com>
2022-03-28 20:32:39 -0300

    fix: fix readme and seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,38 @@
+import { HashService } from '../src/shared/hash/hash.service';
+import { UserType } from '../src/user/types/user-type.enum';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
+const hashService = new HashService();
 
 async function main() {
-  await prisma.userType.createMany({
+  const clientType = await prisma.userType.create({
+    data: {
+      type: UserType.CLIENT,
+    },
+  });
+
+  const professionalType = await prisma.userType.create({
+    data: {
+      type: UserType.PROFESSIONAL,
+    },
+  });
+
+  const password = await hashService.generateHash('123123');
+
+  await prisma.user.createMany({
     data: [
       {
-        type: 'Professional',
+        name: 'John Doe',
+        email: 'john@doe.com',
+        password,
+        userTypeId: clientType.id,
       },
       {
-        type: 'Client',
+        name: 'Jane Doe',
+        email: 'jane@doe.com',
+        password,
+        userTypeId: professionalType.id,
       },
     ],
   });
