0c5e3264444eca3b90c6c35897f95fca13a67ba4
Gustavo Oquendo Nogales <gustavo.nogales@hotmail.com>
2022-03-28 20:54:31 -0300

    change default port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(parseInt(process.env.PORT));
 }
 bootstrap();
