aee85dcbf6d83b394b1c66b0e00d23cb22e8ce9c
Gustavo Oquendo Nogales <gustavo.nogales@hotmail.com>
2022-04-09 17:16:19 -0300

    feat: add cache manager

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,25 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { AuthModule } from './shared/auth/auth.module';
 import { SharedModule } from './shared/shared.module';
 import { AppointmentModule } from './appointment/appointment.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [UserModule, SharedModule, AppointmentModule],
+  imports: [
+    CacheModule.register({
+      isGlobal: true,
+      ttl: 10,
+      max: 10,
+    }),
+    UserModule,
+    SharedModule,
+    AppointmentModule,
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
